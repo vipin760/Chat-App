ad5d29a16b1775f0dab4e16c0cc833084080053e
vipin_760 <vipinm500@gmail.com>
2024-08-06 17:36:01 +0530

    endponit addef

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,7 @@
-export const USER_API_URI = 'http://localhost:3000/api/v2'
+// export const USER_API_URI = 'http://localhost:3000/api/v2'
 
-export const END_POINT = 'http://localhost:3000'
+// export const END_POINT = 'http://localhost:3000'
+
+export const USER_API_URI = 'https://chat-app-rz6n.onrender.com/api/v2'
+
+export const END_POINT = 'https://chat-app-rz6n.onrender.com'
